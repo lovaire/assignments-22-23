e0bd96f1fc35c4f77497b3cd0b52e7c1d58c7f60
lovaire <124887977+lovaire@users.noreply.github.com>
2023-03-02 21:51:57 +0700

    Update NotaGenerator.java

--- assignment1/src/main/java/assignments/assignment1/NotaGenerator.java
+++ assignment1/src/main/java/assignments/assignment1/NotaGenerator.java
@@ -1,66 +1,76 @@
-package assignments.assignment1;
-
 import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class NotaGenerator {
-    private static final Scanner input = new Scanner(System.in);
-
-    /**
-     * Method main, program utama kalian berjalan disini.
-     */
     public static void main(String[] args) {
-        // TODO: Implement interface menu utama
-    }
+        Scanner input = new Scanner(System.in);
+        String nama, hp, paket;
+        int pilihan, berat, harga;
+        LocalDate tanggalTerima, tanggalSelesai;
 
-    /**
-     * Method untuk menampilkan menu di NotaGenerator.
-     */
-    private static void printMenu() {
         System.out.println("Selamat datang di NotaGenerator!");
-        System.out.println("==============Menu==============");
-        System.out.println("[1] Generate ID");
-        System.out.println("[2] Generate Nota");
-        System.out.println("[0] Exit");
-    }
+        do {
+            System.out.println("==============Menu==============");
+            System.out.println("[1] Generate ID");
+            System.out.println("[2] Generate Nota");
+            System.out.println("[0] Exit");
+            System.out.print("Pilihan : ");
+            pilihan = input.nextInt();
+            input.nextLine(); 
 
-    /**
-     * Method untuk menampilkan paket.
-     */
-    private static void showPaket() {
-        System.out.println("+-------------Paket-------------+");
-        System.out.println("| Express | 1 Hari | 12000 / Kg |");
-        System.out.println("| Fast    | 2 Hari | 10000 / Kg |");
-        System.out.println("| Reguler | 3 Hari |  7000 / Kg |");
-        System.out.println("+-------------------------------+");
-    }
+            if (pilihan == 1) {
+                System.out.println("================================");
+                System.out.print("Masukkan nama Anda: ");
+                nama = input.nextLine();
+                System.out.print("Masukkan nomor handphone Anda: ");
+                hp = input.nextLine();
+                String id = generateID(nama, hp);
+                System.out.println("ID Anda : " + id);
+            } else if (pilihan == 2) {
+                System.out.println("================================");
+                System.out.print("Masukkan nama Anda: ");
+                nama = input.nextLine();
+                System.out.print("Masukkan nomor handphone Anda: ");
+                hp = input.nextLine();
+                System.out.print("Masukkan tanggal terima (dd/mm/yyyy): ");
+                tanggalTerima = LocalDate.parse(input.nextLine(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+                System.out.print("Masukkan paket laundry: ");
+                paket = input.nextLine();
+                while (!isValidPaket(paket)) {
+                    System.out.println("Paket " + paket + " tidak diketahui");
+                    System.out.println("[ketik ? untuk mencari tahu jenis paket]");
+                    System.out.print("Masukkan paket laundry: ");
+                    paket = input.nextLine();
+                    if (paket.equals("?")) {
+                        showPaket();
+                    }
+                }
+                System.out.print("Masukkan berat cucian Anda [Kg]: ");
+                while (!input.hasNextInt()) {
+                    System.out.println("Harap masukkan berat cucian Anda dalam bentuk bilangan positif.");
+                    System.out.print("Masukkan berat cucian Anda [Kg]: ");
+                    input.next();
+                }
+                berat = input.nextInt();
+                berat = (berat < 2) ? 2 : berat;
+                harga = hitungHarga(paket, berat);
+                tanggalSelesai = tanggalTerima.plusDays(getWaktuProses(paket));
+                String id = generateID(nama, hp);
+                System.out.println("Nota Laundry");
+                System.out.println("ID    : " + id);
+                System.out.println("Paket : " + paket);
+                System.out.println("Harga :");
+                System.out.println(berat + " kg x " + harga/berat + " = " + harga);
+                System.out.println("Tanggal Terima  : " + tanggalTerima.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+                System.out.println("Tanggal Selesai : " + tanggalSelesai.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+            } else if (pilihan != 0) {
+                System.out.println("================================");
+                System.out.println("Perintah tidak diketahui, silakan periksa kembali.");
+            }
+        } while (pilihan != 0);
 
-    /**
-     * Method untuk membuat ID dari nama dan nomor handphone.
-     * Parameter dan return type dari method ini tidak boleh diganti agar tidak mengganggu testing
-     *
-     * @return String ID anggota dengan format [NAMADEPAN]-[nomorHP]-[2digitChecksum]
-     */
-    public static String generateId(String nama, String nomorHP){
-        // TODO: Implement generate ID sesuai soal.
-        return null;
-    }
-
-    /**
-     *
-     * Method untuk membuat Nota.
-     * Parameter dan return type dari method ini tidak boleh diganti agar tidak mengganggu testing.
-     *
-     * @return string nota dengan format di bawah:
-     *         <p>ID    : [id]
-     *         <p>Paket : [paket]
-     *         <p>Harga :
-     *         <p>[berat] kg x [hargaPaketPerKg] = [totalHarga]
-     *         <p>Tanggal Terima  : [tanggalTerima]
-     *         <p>Tanggal Selesai : [tanggalTerima + LamaHariPaket]
-     */
-
-    public static String generateNota(String id, String paket, int berat, String tanggalTerima){
-        // TODO: Implement generate nota sesuai soal.
-        return null;
+        System.out.println("================================");
+        System.out.println("Terima kasih telah menggunakan NotaGenerator!");
     }
 }
